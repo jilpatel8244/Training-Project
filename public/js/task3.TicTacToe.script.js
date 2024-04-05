1dfdde094aa8b10fd0e04878b014325ed2000ee3
jilpatel8244 <jil.patel.2024@gmail.com>
2024-04-05 14:13:54 +0530

    tic_tac_toe change

--- public/js/task3.TicTacToe.script.js
+++ public/js/task3.TicTacToe.script.js
@@ -1,4 +1,5 @@
 let element = 'x';
+let winnerFlag = false;
 let oClicked = [];
 let xClicked = [];
 let count = 0;
@@ -27,23 +28,24 @@ function clickHandler() {
     }
 
     if ((oClicked.includes("1") && oClicked.includes("2") && oClicked.includes("3")) || (oClicked.includes("4") && oClicked.includes("5") && oClicked.includes("6")) || (oClicked.includes("7") && oClicked.includes("8") && oClicked.includes("9")) || (oClicked.includes("1") && oClicked.includes("4") && oClicked.includes("7")) || (oClicked.includes("2") && oClicked.includes("5") && oClicked.includes("8")) || (oClicked.includes("3") && oClicked.includes("6") && oClicked.includes("9")) || (oClicked.includes("1") && oClicked.includes("5") && oClicked.includes("9")) || (oClicked.includes("3") && oClicked.includes("5") && oClicked.includes("7"))) {
-
+        winnerFlag = true;
         document.getElementById("status").innerHTML = "";
         document.getElementsByClassName('container')[0].innerHTML = "winner is o";
         setTimeout(function () { location.reload() }, 2000);
     }
     else if ((xClicked.includes("1") && xClicked.includes("2") && xClicked.includes("3")) || (xClicked.includes("4") && xClicked.includes("5") && xClicked.includes("6")) || (xClicked.includes("7") && xClicked.includes("8") && xClicked.includes("9")) || (xClicked.includes("1") && xClicked.includes("4") && xClicked.includes("7")) || (xClicked.includes("2") && xClicked.includes("5") && xClicked.includes("8")) || (xClicked.includes("3") && xClicked.includes("6") && xClicked.includes("9")) || (xClicked.includes("1") && xClicked.includes("5") && xClicked.includes("9")) || (xClicked.includes("3") && xClicked.includes("5") && xClicked.includes("7"))) {
-
+        winnerFlag = true;
         document.getElementById("status").innerHTML = "";
         document.getElementsByClassName('container')[0].innerHTML = "winner is x";
         setTimeout(function () { location.reload() }, 2000);
     }
-
-    if (count == 9) {
-        document.getElementById("status").innerHTML = "";
-        document.getElementsByClassName('container')[0].innerHTML = "match drow";
-        setTimeout(function () { location.reload() }, 2000);
-    }
+    if (!winnerFlag) {
+        if (count == 9) {
+            document.getElementById("status").innerHTML = "";
+            document.getElementsByClassName('container')[0].innerHTML = "match drow";
+            setTimeout(function () { location.reload() }, 2000);
+        }
+    } 
 }
 
 
